1ad29e68109dc5f49b0ec1c10fbfea38f62dd5fc
DMG0411 <93600007+DMG0411@users.noreply.github.com>
2025-11-17 22:27:21 +0200

    improvements

--- wedding-invitation/src/app/invitation/invitation.component.ts
+++ wedding-invitation/src/app/invitation/invitation.component.ts
@@ -5,4 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './invitation.component.html',
   styleUrls: ['./invitation.component.scss'],
 })
-export class InvitationComponent {}
+export class InvitationComponent {
+onOpenLocation(): void {
+  const lat = 44.4171237;
+  const lng = 26.028437;
+  const label = encodeURIComponent('Zocalo Ballroom');
+
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+  const isAndroid = /Android/i.test(navigator.userAgent);
+
+  const webUrl = `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`;
+
+  if (isIOS) {
+    const appUrl = `comgooglemaps://?q=${lat},${lng}(${label})`;
+    window.location.href = appUrl;
+
+  } else if (isAndroid) {
+    const appUrl = `geo:${lat},${lng}?q=${lat},${lng}(${label})`;
+    window.location.href = appUrl;
+
+  } else {
+    window.open(webUrl, '_blank');
+  }
+}
+
+}
