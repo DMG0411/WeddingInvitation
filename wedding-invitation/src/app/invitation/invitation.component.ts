d0593cf140ea5cf0f55bd75ff606f58366d7d7e3
DMG0411 <93600007+DMG0411@users.noreply.github.com>
2025-11-23 18:33:20 +0200

    Church location updated.

--- wedding-invitation/src/app/invitation/invitation.component.ts
+++ wedding-invitation/src/app/invitation/invitation.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./invitation.component.scss'],
 })
 export class InvitationComponent {
-onOpenLocation(): void {
+onOpenResturant(): void {
   const lat = 44.4171237;
   const lng = 26.028437;
   const label = encodeURIComponent('Zocalo Ballroom');
@@ -29,4 +29,26 @@ onOpenLocation(): void {
   }
 }
 
+onOpenChurch(): void {
+  const lat = 44.4222131;
+  const lng = 26.1321645;
+  const label = encodeURIComponent('Biserica Sfânta Treime Dudești');
+
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+  const isAndroid = /Android/i.test(navigator.userAgent);
+
+  const webUrl = `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`;
+
+  if (isIOS) {
+    const appUrl = `comgooglemaps://?q=${lat},${lng}(${label})`;
+    window.location.href = appUrl;
+
+  } else if (isAndroid) {
+    const appUrl = `geo:${lat},${lng}?q=${lat},${lng}(${label})`;
+    window.location.href = appUrl;
+
+  } else {
+    window.open(webUrl, '_blank');
+  }
+}
 }
